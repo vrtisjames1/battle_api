666655003dec87236fb8e1a7a22238813aa5f49f
James Vrtis <jamesvrtis@caskradmins-mbp.mynetworksettings.com>
2023-01-12 12:06:23 -0500

    updated stat

--- game/serializers.py
+++ game/serializers.py
@@ -24,4 +24,14 @@ class GamesSerializer(serializers.ModelSerializer): # serializers.ModelSerialize
         'g1_2','g2_2','g3_2','g4_2','g5_2','g6_2','g7_2','g8_2','g9_2','g10_2',
         'h1_2','h2_2','h3_2','h4_2','h5_2','h6_2','h7_2','h8_2','h9_2','h10_2',
         'i1_2','i2_2','i3_2','i4_2','i5_2','i6_2','i7_2','i8_2','i9_2','i10_2',
-        'j1_2','j2_2','j3_2','j4_2','j5_2','j6_2','j7_2','j8_2','j9_2','j10_2',) 
+        'j1_2','j2_2','j3_2','j4_2','j5_2','j6_2','j7_2','j8_2','j9_2','j10_2',
+        'stat_a1','stat_a2','stat_a3','stat_a4','stat_a5','stat_a6','stat_a7','stat_a8','stat_a9','stat_a10',
+        'stat_b1','stat_b2','stat_b3','stat_b4','stat_b5','stat_b6','stat_b7','stat_b8','stat_b9','stat_b10',
+        'stat_c1','stat_c2','stat_c3','stat_c4','stat_c5','stat_c6','stat_c7','stat_c8','stat_c9','stat_c10',
+        'stat_d1','stat_d2','stat_d3','stat_d4','stat_d5','stat_d6','stat_d7','stat_d8','stat_d9','stat_d10',
+        'stat_e1','stat_e2','stat_e3','stat_e4','stat_e5','stat_e6','stat_e7','stat_e8','stat_e9','stat_e10',
+        'stat_f1','stat_f2','stat_f3','stat_f4','stat_f5','stat_f6','stat_f7','stat_f8','stat_f9','stat_f10',
+        'stat_g1','stat_g2','stat_g3','stat_g4','stat_g5','stat_g6','stat_g7','stat_g8','stat_g9','stat_g10',
+        'stat_h1','stat_h2','stat_h3','stat_h4','stat_h5','stat_h6','stat_h7','stat_h8','stat_h9','stat_h10',
+        'stat_i1','stat_i2','stat_i3','stat_i4','stat_i5','stat_i6','stat_i7','stat_i8','stat_i9','stat_i10',
+        'stat_j1','stat_j2','stat_j3','stat_j4','stat_j5','stat_j6','stat_j7','stat_j8','stat_j9','stat_j10',) 
